71df24f90daf7a12e465afa2a931b636e7d6f93a
boilerbill83 <boilerbill@gmail.com>
2026-04-02 09:44:38 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/fishers-house-search-2026/",
+    publicPath: "/",
     clean: true,
   },
   module: {
